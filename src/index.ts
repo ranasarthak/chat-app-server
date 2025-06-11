82149a0ee5599dde1ccfe2db7209eecb70b07a1e
ranasarthak <sara.ran95@gmail.com>
2025-06-12 02:00:12 +0530

    v0 chat app server is ready

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
-import express from 'express';
+import { error } from "console";
+import { WebSocketServer, WebSocket } from "ws";
 
-const app = express();
-const PORT = process.env.port || 3000;
+const wss = new WebSocketServer({ port: 8080 });
 
-app.use(express.json());
+function broadcast(message: string) {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
 
-app.listen(PORT, () => {
-    console.log("Server started at port: ", PORT);
+wss.on('connection', (socket: WebSocket) => {
+    broadcast(`New user joined. Total no of users are: ${wss.clients.size}`);
+
+    socket.on("message", (message: string) => {
+        broadcast(message.toString());
+    })
+
+    socket.on('close', () => {
+        broadcast(`User left the chat. Total no of users are: ${wss.clients.size}`);
+    })
+
+    socket.on('error', (error) => {
+        console.error("Error encountered.", error);
+    })
 })
+
+console.log("WebSocketServer is running on: 8080");
